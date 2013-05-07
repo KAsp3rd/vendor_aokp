1d7d92c5657a1fd73bfdf4e69d6af93a1099b7fd
Donald Sears <kasp3rd@gmail.com>
2013-05-07 11:06:04 -0400

    Bring personal vendor file up to date
    Change-Id: If9b64bb416e17c5f86b1af9cfb07daa7808aa0fa

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,6 +1,7 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
+GDATE= $(shell date '+%Y%m%d')
 DATE = $(shell vendor/aokp/tools/getdate)
 
 ifneq ($(AOKP_BUILD),)
@@ -10,16 +11,22 @@ ifneq ($(AOKP_BUILD),)
         ro.goo.rom=aokp \
         ro.goo.version=$(shell echo $(AOKP_BUILD) | cut -d/ -f1) \
         ro.aokp.version=$(TARGET_PRODUCT)_jb-mr1_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
-else
-    ifneq ($(AOKP_NIGHTLY),)
-        # AOKP_NIGHTLY=true
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.aokp.version=$(TARGET_PRODUCT)_nightly_$(DATE)
-    else
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.aokp.version=$(TARGET_PRODUCT)_unofficial_$(DATE)
-    endif
 endif
+
+ifneq ($(MY_BUILD),)
+        PRODUCT_PROPERTY_OVERRIDES += \
+	    ro.goo.developerid=KAsp3rd \
+	    ro.goo.rom=aokp \
+	    ro.goo.version= $(GDATE) \
+		ro.aokp.version=$(TARGET_PRODUCT)_jb-mr1_$(DATE)
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.goo.developerid=KAsp3rd \
+        ro.goo.rom=aokp_mr1_testing \
+        ro.goo.version=0 \
+            ro.aokp.version=$(TARGET_PRODUCT)_jb-mr1_test
+endif
+
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
